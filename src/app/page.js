b84ef3abc051fcb0b2d6a27dbf7e84794bcc7ea1
Oebus <oliverw4321@gmail.com>
2023-06-30 00:29:38 +1200

    fix typo

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
         <div>
             <h1>Welcome To Power Up Forest</h1>
             <h6>A helpful time management application to help you with productivity and goal achievement</h6>
-            <div class="box">
+            <div className="box">
             <h5>Where would you like to start your journey?</h5>
                 <a href="/signup">SIGN UP</a><br/><br/>
                 <a href="/login">LOG IN</a>
